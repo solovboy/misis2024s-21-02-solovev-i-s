31773f125290eb485286d98386a0653b0dc6b307
Иван Соловьев <msksolovev93@mail.ru>
2024-03-11 16:22:43 +0300

    Добавил функцию бинаризации Ниблека и возможность консольного запуска

--- prj.lab/lab04/lab04.cpp
+++ prj.lab/lab04/lab04.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <omp.h>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+
+
+cv::Mat niblackBinary(const cv::Mat& image, int windowSize, double k) {
+	cv::Mat binary(image.size(), CV_8U, cv::Scalar(0));
+
+	int halfWindowSize = windowSize / 2;
+
+	#pragma omp parallel for collapse(2)
+	for (int y = halfWindowSize; y < image.rows - halfWindowSize; ++y) {
+		for (int x = halfWindowSize; x < image.cols - halfWindowSize; ++x) {
+			cv::Rect windowRect(x - halfWindowSize, y - halfWindowSize, windowSize, windowSize);
+			cv::Mat window = image(windowRect);
+
+			// Calculate the average value and standard deviation in the window
+			cv::Scalar mean, stddev;
+			cv::meanStdDev(window, mean, stddev);
+
+			double threshold = mean.val[0] + k * stddev.val[0];
+
+			// If the contrast is greater than the threshold, then the pixel turns white, otherwise black
+			if (image.at<uchar>(y, x) > threshold)
+				binary.at<uchar>(y, x) = 255;
+			else
+				binary.at<uchar>(y, x) = 0;
+		}
+	}
+
+	return binary;
+}
 
 cv::Mat bernsenBinary(const cv::Mat& image, int windowSize, int contrastThreshold) {
 	cv::Mat binary(image.size(), CV_8U, cv::Scalar(0));
@@ -9,19 +39,20 @@ cv::Mat bernsenBinary(const cv::Mat& image, int windowSize, int contrastThreshol
 	int halfWindowSize = windowSize / 2;
 	int contrastMax = 255;
 
+	#pragma omp parallel for collapse(2)
 	for (int y = halfWindowSize; y < image.rows - halfWindowSize; ++y) {
 		for (int x = halfWindowSize; x < image.cols - halfWindowSize; ++x) {
 			cv::Rect windowRect(x - halfWindowSize, y - halfWindowSize, windowSize, windowSize);
 			cv::Mat window = image(windowRect);
 
-			// Ќаходим минимальное и максимальное значение в окне
+			// Find the minimum and maximum values in the window
 			double minVal, maxVal;
 			cv::minMaxLoc(window, &minVal, &maxVal);
 
-			// ¬ычисл€ем контраст окна
+			// Calculating the window contrast
 			double contrast = maxVal - minVal;
 
-			// ≈сли контраст больше порога, то пиксель становитс€ белым, иначе черным
+			// If the contrast is greater than the threshold, then the pixel turns white, otherwise black
 			if (contrast >= contrastThreshold)
 				binary.at<uchar>(y, x) = contrastMax;
 			else
@@ -55,13 +86,32 @@ cv::Mat generateImage(const int& countCircles, int minRadius, int maxRadius, int
 }
 
 
-int main() {
+int main(int argc, char* argv[]) {
+	//Default parametrs
+	int countCircles = 6;
+	int minRadius = 15;
+	int maxRadius = 30;
+	int minContrast = 50;
+	int maxContrast = 255;
+	int blur = 4;
 
-	cv::Mat image = generateImage(6, 15, 30, 50, 255, 4);
+	if (argc >= 7) {
+		countCircles = std::stoi(argv[1]);
+		minRadius = std::stoi(argv[2]);
+		maxRadius = std::stoi(argv[3]);
+		minContrast = std::stoi(argv[4]);
+		maxContrast = std::stoi(argv[5]);
+		blur = std::stoi(argv[6]);
+	}
 
-	cv::Mat binary = bernsenBinary(image, 3, 3);
+	cv::Mat image = generateImage(countCircles, minRadius, maxRadius, minContrast, maxContrast, blur);
 
-	cv::imshow("Image1", image);
-	cv::imshow("Image2", binary);
+	cv::Mat binaryBernsen = bernsenBinary(image, 3, 3);
+
+	cv::Mat binaryNiblack = niblackBinary(image, 3, -0.2);
+
+	cv::imshow("Original", image);
+	cv::imshow("binaryBernsen", binaryBernsen);
+	cv::imshow("binaryNiblack", binaryNiblack);
 	cv::waitKey(0);
 }
