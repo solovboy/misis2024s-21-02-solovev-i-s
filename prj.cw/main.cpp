115884a8c658e179ec5aed711a5c01817b709641
Иван Соловьев <msksolovev93@mail.ru>
2024-03-28 21:45:45 +0300

    Добавил вычисление метрики PSNR

--- prj.cw/main.cpp
+++ prj.cw/main.cpp
@@ -7,13 +7,32 @@
 #include <string>
 
 
+double getPSNR(const cv::Mat& I1, const cv::Mat& I2)
+{
+    cv::Mat s1;
+    absdiff(I1, I2, s1);       
+    s1.convertTo(s1, CV_32F);  
+    s1 = s1.mul(s1);         
+
+    cv::Scalar s = sum(s1);         
+    double sse = s.val[0]; 
+
+    if (sse <= 1e-10) 
+        return 0;
+    else
+    {
+        double  mse = sse / (double)(I1.channels() * I1.total());
+        double psnr = 10.0 * log10((255 * 255) / mse);
+        return psnr;
+    }
+}
 
 int main(int argc, char* argv[]) {
     try {
-        std::string file_path = "C:/Users/Иван/misis2024s-21-02-solovev-i-s/prj.cw/tomography.png";
+        std::string file_path = "C:/Users/Иван/misis2024s-21-02-solovev-i-s/prj.cw/livingroom.png";
         double a = 0.2;
         int T = 10;
-        double k = 8;
+        double k = 12;
         if (argc >= 5) {
             file_path = argv[1];
             a = std::stof(argv[2]);
@@ -24,11 +43,14 @@ int main(int argc, char* argv[]) {
             throw argc;
         }
         
-        cv::Mat I = cv::imread(file_path, cv::IMREAD_GRAYSCALE);
-        cv::imshow("Original image", I);
-
-        PeronaMalik image(I, a, T, k);
+        const cv::Mat I = cv::imread(file_path, cv::IMREAD_GRAYSCALE);
+        cv::Mat I1 = cv::imread(file_path, cv::IMREAD_GRAYSCALE);
+        PeronaMalik image(I1, a, T, k);
         cv::Mat Processed_image = image.PeronaMalikGray();
+
+        double getpsnr = getPSNR(I, Processed_image);
+
+        cv::imshow("Original image", I);
         cv::imshow("Processed image", Processed_image);
         cv::waitKey(0);
 
